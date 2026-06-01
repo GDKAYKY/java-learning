7cc2a378eca8aba647536b1404b76de35b13ec85
GDKAYKY <gdkayky@proton.me>
2026-06-01 00:02:49 -0300

    Add Java learning exercises covering basic concepts

---
+++ src/Profile.java
@@ -0,0 +1,6 @@
+public class Profile {
+    public static void main(String[] args) {
+        // System.out.println("Whats up!, my name's Kayky");
+        System.out.println("Whats up, is everything good?");
+    }
+}
---
+++ src/ScreenMatch.java
@@ -0,0 +1,21 @@
+public class ScreenMatch {
+    public static void main(String[] args) {
+        /*
+         * System.out.println("This is a screen match!");
+         * System.out.println("Top Gun Maverick");
+         * 
+         * int year;
+         * year = 2022;
+         * 
+         * System.out.print("The Film was released in" + year);
+         * 
+         * boolean includedInCurrentPlan = true;
+         * System.out.println("Is the film included in the current plan? " +
+         * includedInCurrentPlan);
+         */
+        int num = 5;
+        int result = --num;
+
+        System.out.println(num);
+    }
+}
